6d4c2d17e104c74679146a7613d87aad3da94fb6
Your Name <you@example.com>
2018-04-09 18:18:01 +0800

    修改php版本

--- src/Lock/RedisLock.php
+++ src/Lock/RedisLock.php
@@ -147,8 +147,9 @@ class RedisLock extends LockInterface
     private function initRedis($config)
     {
         $this->redis = new Client([
-            'host'   => $config['host'] ? $config['host'] : '127.0.0.1',
-            'port'   => $config['port'] ? $config['port'] : '6379'
+            'host'       => $config['host'] ? $config['host'] : '127.0.0.1',
+            'port'       => $config['port'] ? $config['port'] : '6379',
+            'password'   => $config['password'] ? $config['password'] : ''
         ]);
     }
 
